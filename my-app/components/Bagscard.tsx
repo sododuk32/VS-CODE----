6f17aa6ad30f3d5d274db532c9ad13809cac48b0
sododuk32 <37325163+sododuk32@users.noreply.github.com>
2022-10-04 17:42:27 +0900

    장바구니 페이지 , 장바구니 상품 카드.,
    react-cookie 모듈의 get이 사용불가능함 이유찾자

---
+++ my-app/components/Bagscard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './Bagscard.module.css';
+
+function Bagscard(
+    photoName: string,
+    productName: string,
+    amount: string,
+    price: string
+) {
+    return (
+        <div>
+            <div id="cardContainer" className={styles.cardContainer}>
+                <div id="img">{photoName}</div>
+                <div id="cardDetail" className={styles.cardDetail}>
+                    <div id="productInfo" className={styles.productInfo}>
+                        <div id="productName" className={styles.productName}>
+                            {photoName}
+                        </div>
+                        <div
+                            id="productAmount"
+                            className={styles.productAmount}
+                        >
+                            {amount}
+                        </div>
+                        <div id="productPrice" className={styles.productPrice}>
+                            {price}
+                        </div>
+                    </div>
+                    <div id="addMessage">Add a gift message or gift wrap</div>
+                    <div id="zipCode">
+                        Find out how soon you can get this item.
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Bagscard;
--- my-app/pages/UsersBag.tsx
+++ my-app/pages/UsersBag.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
-
+import Nav from '../components/Nav';
+import Bagscard from '../components/Bagscard';
+import styles from './UsersBag.module.css';
+import cookie from 'react-cookie';
 function UsersBag() {
-    return <div>cssas</div>;
+    const mycookie = cookie.get('cart', true);
+
+    return (
+        <div>
+            <Nav />
+            <div className={styles.bodyContainer}>
+                <div id="someTtext" className={styles.someTtext}>
+                    <h1 id="phrase" className={styles.phrase}>
+                        Review your bag
+                    </h1>
+                    <div id="examin" className={styles.examin}>
+                        Free delivery and free returns
+                    </div>
+                </div>
+                <div id="payingexplane" className={styles.payingexplane}>
+                    Pay $3.25/mo.per month at 0% APRfootnote◊ for eligible items
+                    in your order when you choose Apple Card Monthly
+                    Installments at checkout.
+                </div>
+
+                <div id="cardContainer">
+                    {Bagscard('ph2', 'name2', 'amount2', 'price2')}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default UsersBag;
