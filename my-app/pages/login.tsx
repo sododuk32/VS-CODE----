2d479c3ed33e66efd5cc1f8b009b2813c1bf8e52
sododuk32 <37325163+sododuk32@users.noreply.github.com>
2022-10-11 00:04:31 +0900

    post 통신의 값이 object임에도 json으로 전달이 안댐

--- my-app/pages/login.tsx
+++ my-app/pages/login.tsx
@@ -1,42 +1,62 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 function login() {
-    let ttt = '';
+    const [jwtk, setjwtk] = useState('');
+    const [usersid, setID] = useState('');
+    const [userspw, setPW] = useState('');
+
     function sendingInfo() {
         //서버에서 받아본 결과 req.params에 아무것도 안들어있음
         // 값을 정확하게 보내줄수있도록 코드 변경하기
         axios
             .post('http://localhost:8080/login', {
-                usersid: document.getElementById('inputId').value,
-                userspw: document.getElementById('inputPw').value,
+                usersid: usersid,
+                userspw: userspw,
             })
             .then((res) => {
-                ttt = res.data;
-                console.log(ttt);
-                testingToken(ttt);
+                console.log(usersid + 'id || pw' + userspw);
+                console.log(res.data);
+                setjwtk(res.data);
             });
     }
-    function testingToken(ttoken: string) {
-        console.log('parameter' + ttoken);
+    let verifys = '';
+    function verifyss() {
+        console.log('parameter' + jwtk);
+
         axios
             .post('http://localhost:8080/verify', {
-                mytoken: ttoken,
+                mytoken: jwtk,
             })
             .then((res) => {
+                console.log(jwtk);
                 console.log(res.data);
+                return (verifys = res.data);
             });
     }
+
     return (
         <div>
             <div id="loginContainer">
                 <h1 id="signText">Sign in for faster checkout.</h1>
                 <div id="inputContainer">
                     <span>Sign in to Apple Store</span>
-                    <input id="inputId" placeholder="Apple-ID" type="text" />
-                    <input id="inputPw" placeholder="Password" type="text" />
-                    <button onClick={sendingInfo}>login</button>
+                    <input
+                        id="inputId"
+                        placeholder="Apple-ID"
+                        type="text"
+                        onChange={(event) => setID(event.target.value)}
+                    />
+                    <input
+                        id="inputPW"
+                        placeholder="Apple-PW"
+                        type="text"
+                        onChange={(event) => setPW(event.target.value)}
+                    />
+                    <button onClick={sendingInfo}>Login</button>
+                    <button onClick={verifyss}>Login</button>
                 </div>
             </div>
         </div>
