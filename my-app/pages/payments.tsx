4e787498b1eee254ef2851ac056242e448409c15
sododuk32 <37325163+sododuk32@users.noreply.github.com>
2022-10-22 23:32:11 +0900

    dd

--- my-app/pages/payments.tsx
+++ my-app/pages/payments.tsx
@@ -5,6 +5,7 @@ import React, { useRef } from 'react';
 import Nav from '../components/Nav';
 import { useState } from 'react';
 import Selectshipping from '../components/Selectshipping';
+import Selecttakeoff from '../components/Selecttakeoff';
 import { useEffect } from 'react';
 import { Cookies } from 'react-cookie';
 import useCookies from 'react-cookie';
